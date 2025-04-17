4af8d73d57f3b366e7d3dd24f273b9ddf4f49ecd
ravi <rteja4343@gmail.com>
2025-04-17 22:18:53 +0530

    advas

--- loop.java
+++ loop.java
@@ -7,7 +7,7 @@ public class loop {
     public static void main(String[] args) throws IOException {
        
 Scanner sc = new Scanner(System.in);
-
+System.out.println("enter a number");
         int N = sc.nextInt();
         for (int i = 1; i <= 10; i++) {
             int result = N * i;
